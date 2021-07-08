04807f7484660be7ca96a4f72198149e10cbc741
Jarlei <jarlei_rodrigues@outlook.com>
2021-07-08 09:05:21 -0300

    test: Header component

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   testPathIgnorePatterns: ["/node-modules", "/.next", "/.vercel"],
-  setupFilesAfterEnv: [
-    "<rootDir>/src/tests/setupTests.ts"
-  ],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
-  testEnvironment: 'jsdom'
-}
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy",
+  },
+};
--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,34 +1,34 @@
-import { render } from '@testing-library/react';
-import { ActiveLink } from '.';
+import { render, screen } from "@testing-library/react";
+import { ActiveLink } from ".";
 
-jest.mock('next/router', () => {
+jest.mock("next/router", () => {
   return {
     useRouter() {
       return {
-        asPath: '/'
-      }
-    }
-  }
+        asPath: "/",
+      };
+    },
+  };
 });
 
-describe('active link component', () => {
-  it('renders correctly', () => {
-    const { getByText } = render(
-      <ActiveLink href='/' activeClassName="active">
+describe("active link component", () => {
+  it("renders correctly", () => {
+    render(
+      <ActiveLink href='/' activeClassName='active'>
         <a>Home</a>
       </ActiveLink>
-    )
-  
-    expect(getByText('Home')).toBeInTheDocument()
+    );
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
   });
-  
-  it('adds active class if the link is currently active', () => {
-    const { getByText } = render(
-      <ActiveLink href='/' activeClassName="active">
+
+  it("adds active class if the link is currently active", () => {
+    render(
+      <ActiveLink href='/' activeClassName='active'>
         <a>Home</a>
       </ActiveLink>
-    )
-  
-    expect(getByText('Home')).toHaveClass('active')
+    );
+
+    expect(screen.getByText("Home")).toHaveClass("active");
   });
-})
+});
